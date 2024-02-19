0e2e06cee80deac8073f4ec7bb4a6de42875bcb2
bronivichok1 <lazarenkov01@gmail.com>
2024-02-19 14:53:58 +0300

    dev 0.0.2

--- SoungsHz/Form1.Designer.cs
+++ SoungsHz/Form1.Designer.cs
@@ -37,11 +37,25 @@ namespace SoungsHz
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(12, 40);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(86, 36);
             this.button1.TabIndex = 0;
@@ -51,7 +65,7 @@ namespace SoungsHz
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(104, 12);
+            this.button2.Location = new System.Drawing.Point(104, 40);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(86, 36);
             this.button2.TabIndex = 1;
@@ -61,7 +75,7 @@ namespace SoungsHz
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(196, 12);
+            this.button3.Location = new System.Drawing.Point(196, 40);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(86, 36);
             this.button3.TabIndex = 2;
@@ -71,7 +85,7 @@ namespace SoungsHz
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(288, 12);
+            this.button4.Location = new System.Drawing.Point(288, 40);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(86, 36);
             this.button4.TabIndex = 3;
@@ -81,7 +95,7 @@ namespace SoungsHz
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(380, 12);
+            this.button5.Location = new System.Drawing.Point(380, 40);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(86, 36);
             this.button5.TabIndex = 4;
@@ -91,7 +105,7 @@ namespace SoungsHz
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(472, 12);
+            this.button6.Location = new System.Drawing.Point(472, 40);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(86, 36);
             this.button6.TabIndex = 5;
@@ -101,7 +115,7 @@ namespace SoungsHz
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(564, 12);
+            this.button7.Location = new System.Drawing.Point(564, 40);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(86, 36);
             this.button7.TabIndex = 6;
@@ -111,7 +125,7 @@ namespace SoungsHz
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(73, 242);
+            this.textBox1.Location = new System.Drawing.Point(35, 218);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
@@ -120,11 +134,150 @@ namespace SoungsHz
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(104, 375);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(86, 57);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Start";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(207, 375);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 57);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Stop";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1031, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(196, 215);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "+10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(196, 244);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "-10";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(277, 215);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 13;
+            this.button12.Text = "+1";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(277, 244);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 14;
+            this.button13.Text = "-1";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(35, 244);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 15;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Hz";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 251);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Db";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(667, 490);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(90, 40);
+            this.button14.TabIndex = 18;
+            this.button14.Text = "Gamma 0";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(763, 490);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(90, 40);
+            this.button15.TabIndex = 19;
+            this.button15.Text = "Gamma -30";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1031, 577);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
@@ -133,9 +286,13 @@ namespace SoungsHz
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,6 +308,19 @@ namespace SoungsHz
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
     }
 }
 
--- SoungsHz/Form1.cs
+++ SoungsHz/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,59 +14,201 @@ namespace SoungsHz
 {
     public partial class Form1 : Form
     {
+
+        SoundPlayer player = null;
+        string fileName = string.Empty;
+        string Hz = "50";
+        string Db = "0";
+        string[] Gamma0 = {"16.35","32.7","65.41","130.8","261.6","523.3","1047","2093","4186","4435","4899","4978","5274","5588","5920","6272","6645","7040","7459","7902","10000","15000","20000"};
         public Form1()
         {
             InitializeComponent();
         }
 
-        int Hz=0;
-        public int HzFun(int x)
+        public void HzFun(string x)
         {
-            return Hz = x;
+            Hz = x;
+            textBox1.Text = Hz;
+        }
+        public void DbFun(int x=0) 
+        {
+            int SumDb = Convert.ToInt32(Db) + x;
+            if (SumDb <= 0 && SumDb >= -144)
+            {
+                Db = Convert.ToString(SumDb);
+            }
+            else {
+                Db = Db;
+            }
+            
+            textBox2.Text = Db;
+
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            player = new SoundPlayer();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            HzFun(50);
+            HzFun("50");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            HzFun(100);
+
+            HzFun("100");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            HzFun(1000);
+            HzFun("1000");
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            HzFun(3000);
+
+            HzFun("3000");
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            HzFun(5000);
+
+            HzFun("5000");
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            HzFun(10000);
+            HzFun("10000");
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            HzFun(20000);
+            HzFun("20000");
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string FileLoc= @"Soungs\" + Hz+"Hz_" + Db + "dBFS_5s.wav";
+                player.SoundLocation = FileLoc;
+                player.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            OpenFileDialog oFD = new OpenFileDialog()
+                {
+                    Filter = "WAV|*.wav",
+                    Multiselect = false,
+                    ValidateNames = true
+                };
+            if (oFD.ShowDialog() == DialogResult.OK) 
+                {
+                fileName = oFD.FileName;
+                }
+        }
+
+
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            DbFun(10);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            DbFun(-10);
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+            DbFun(1);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            DbFun(-1);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            for (int i=0;i<23; i++) {
+                HzFun(Gamma0[i]);
+                DbFun(0);
+
+                textBox1.Text = Hz;
+                textBox2.Text = Db;
+
+                Thread.Sleep(1000);
+                try
+                {
+                    string FileLoc = @"Soungs\Gamma0\" + Hz + "Hz_" + Db + "dBFS_1s.wav";
+                    player.SoundLocation = FileLoc;
+                    player.Play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+            }
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+           
+
+            for (int i = 0; i < 23; i++)
+            { 
+                HzFun(Gamma0[i]);
+                textBox1.Text = Hz;
+                textBox2.Text = Db;
+
+                Thread.Sleep(1000);
+                try
+                {
+                    string FileLoc = @"Soungs\Gamma-30\" + Hz + "Hz_" + Db + "dBFS_1s.wav";
+                    player.SoundLocation = FileLoc;
+                    player.Play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
         }
     }
 }
